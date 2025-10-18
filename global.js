ef2cdfe2b5a58236210ec087b1c1b1a2880150bb
Marcushi <yidong0208@gmail.com>
2025-10-17 20:05:40 -0700

    Add JavaScript for automatic navigation menu and current page highlight (Lab 3 Step 3)

---
+++ global.js
@@ -0,0 +1,42 @@
+// global.js
+console.log("IT'S ALIVE!");
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/YidongShi', title: 'GitHub' }
+];
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/'
+    : '/DSC106_LAB01/';
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url.startsWith('http') ? p.url : BASE_PATH + p.url;
+  let title = p.title;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+
+  nav.append(a);
+}
