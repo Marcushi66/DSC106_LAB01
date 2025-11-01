508a2d375c3cb30a8f21b72bc5799dbb1bcd07b5
Marcushi <yidong0208@gmail.com>
2025-11-01 15:57:29 -0700

    Turning the pie into filtering UI for our projects

--- projects/projects.js
+++ projects/projects.js
@@ -4,6 +4,9 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 let projects = [];
 let query = '';
+let selectedIndex = -1;
+let HIGHLIGHT_COLOR = '#FF00A1';
+let lastPieData = [];
 
 (async function initProjects() {
   try {
@@ -63,7 +66,7 @@ let query = '';
 
 // ===== D3 Pie Plot & Legend =====
 const svg = d3.select('#projects-pie-plot');
-const colors = d3.scaleOrdinal(d3.schemeTableau10);
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 // Render pie chart based on given data
 function renderPieChart(data) {
@@ -71,11 +74,27 @@ function renderPieChart(data) {
   const arcData = sliceGenerator(data);
   const arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-  svg.selectAll('path')
+  const paths = svg.selectAll('path')
     .data(arcData, d => d.data.label)
     .join('path')
     .attr('d', arcGenerator)
-    .attr('fill', (_d, i) => colors(i));
+    .attr('fill', (_d, i) => (i === selectedIndex ? HIGHLIGHT_COLOR : colors(i)))
+    .style('cursor', 'pointer');
+
+  paths.on('click', (_evt, d) => {
+    const i = arcData.indexOf(d);
+    selectedIndex = (selectedIndex === i ? -1 : i);
+    const yearLabel = d.data.label;
+    const projectsContainer = document.querySelector('.projects');
+    const filtered =
+      selectedIndex === -1
+        ? projects
+        : projects.filter(p => String(p.year) === String(yearLabel));
+
+    renderProjects(filtered, projectsContainer, 'h2');
+    renderPieAndLegend(projects);
+  });
+
 }
 
 // Render legend based on given data
@@ -86,7 +105,9 @@ function renderLegend(data) {
     .data(data, d => d.label)
     .join('li')
     .attr('class', 'legend-item')
-    .style('--color', (_d, i) => colors(i))
+    .style('--color', (_d, i) =>
+      (i === selectedIndex ? HIGHLIGHT_COLOR : colors(i))
+    )
     .html(d => `<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 }
 
@@ -97,8 +118,7 @@ function renderPieAndLegend(projectsGiven) {
     .map(([year, count]) => ({ label: year, value: count }))
     .sort((a, b) => a.label.localeCompare(b.label));
 
-  d3.select('#projects-pie-plot').selectAll('*').remove();
-  d3.select('.legend').selectAll('*').remove();
+  lastPieData = pieData;
 
   renderPieChart(pieData);
   renderLegend(pieData);
